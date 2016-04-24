18104ac5864ed61d837bfedbd928d25d6618f0ba
Mindi Torrey <minditorrey@me.com>
2016-04-24 11:54:55 -0700

    configure db

---
+++ config/db.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var sqlite3 = require('sqlite3').verbose();
+var path = require('path');
+
+var dbPath = path.join(__dirname, '..data/comments.db');
+
+var db = new sqlite3.Database(dbPath);
+
+module.exports = db;
+
