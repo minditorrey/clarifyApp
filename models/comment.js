aab7353a899a16b0754e24830cddd3273fb2c5c4
Mindi Torrey <minditorrey@me.com>
2016-04-24 13:50:40 -0700

    set model methods

--- models/comment.js
+++ models/comment.js
@@ -1,29 +1,30 @@
 'use strict';
 
 var db = require('../config/db');
+// var moment = require('moment');
 
-db.run('CREATE TABLE IF NOT EXISTS comments (
+db.run(`CREATE TABLE IF NOT EXISTS comments (
 	imageURL TEXT,
 	name TEXT,
-	date DATETIME DEFAULT CURRENT_TIMESTAMP,
 	post TEXT
-	)');
+	)`);
 
 //methods
 
 //facilitates the db-I don't need the db in the route
 exports.get = function(cb) {
-	db.all('SELECT * from comments', cb)''
+	db.all('SELECT * FROM comments', cb)
 };
 
 exports.create = function(comment, cb) {
 	//validation
-	if(!comment.post) {
+	if(!comment.post || !comment.name) {
 		return cb('Please enter comment.')
 	}
 
 	db.run('INSERT INTO comments (imageURL, name, post) VALUES (?, ?, ?)', 
 		comment.imageURL, 
 		comment.name, 
-		comment.post, cb);
+		comment.post, 
+		cb);
 };
--- routes/comments.js
+++ routes/comments.js
@@ -3,10 +3,33 @@
 var express = require('express');
 var router = express.Router();
 
-//actual routes
-router.get('/', (req, res) => {
-	res.send('send all comments back');
-});
+var Comment = require('../models/comment');
+
+//actual routes for get & send:
+router.route('/')
+	.get((req, res) => {
+	//db.run will be calling this
+		Comment.get((err, comments) => {
+			if(err) {
+				return res.status(400).send(err);
+		}
+		res.send(comments);
+	});
+})
+
+	.post((req, res) => {
+	//db.run will be calling this
+
+	//req.body (data object)
+		Comment.create(req.body, err => {
+		if(err) {
+			return res.status(400).send(err);
+		}
+		res.send(todo);
+		});
+	});
+
+
 
 
 module.exports = router;
